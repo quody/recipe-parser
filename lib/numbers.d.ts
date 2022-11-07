aacb972e014abb2e5603de567f9e87c759abe3b1
quody <quody>
2022-11-07 16:03:57 +0200

    add lib for git install

---
+++ lib/numbers.d.ts
@@ -0,0 +1,22 @@
+export declare const engSmall: {
+    [key: string]: number;
+};
+export declare const engMagnitude: {
+    [key: string]: number;
+};
+export declare const itaSmall: {
+    [key: string]: number;
+};
+export declare const itaMagnitude: {
+    [key: string]: number;
+};
+export declare const finSmall: {
+    [key: string]: number;
+};
+export declare const finMagnitude: {
+    [key: string]: number;
+};
+export declare const toTasteMap: {
+    [key: string]: string;
+};
+export declare const numbersMap: Map<any, any>;
---
+++ lib/units.d.ts
@@ -0,0 +1,31 @@
+export declare const engUnits: {
+    [key: string]: string[];
+};
+export declare const finUnits: {
+    [key: string]: string[];
+};
+export declare const engPluralUnits: {
+    [key: string]: string;
+};
+export declare const finPluralUnits: {
+    [key: string]: string;
+};
+export declare const engNameToSymbol: {
+    [key: string]: string;
+};
+export declare const finNameToSymbol: {
+    [key: string]: string;
+};
+export declare const engPreposition: string[];
+export declare const finPreposition: string[];
+export declare const itaUnits: {
+    [key: string]: string[];
+};
+export declare const itaPluralUnits: {
+    [key: string]: string;
+};
+export declare const itaNameToSymbol: {
+    [key: string]: string;
+};
+export declare const itaPreposition: string[];
+export declare const unitsMap: Map<any, any>;
